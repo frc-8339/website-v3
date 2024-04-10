67b6c97d49b709960b7f762f351416866605615e
Toby Cm <toby@vsus.app>
2024-04-09 19:05:28 -0700

    ci: :green_heart:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,6 @@
 import { Box, Text } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
 
 export default function Home() {
-  const isMobile = useMediaQuery("(max-width: 48em)");
-
   return (
     <Box>
       <Text>Home</Text>
