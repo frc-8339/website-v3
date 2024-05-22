0cbd2d30632507653ebd7aa52221956ac089ccb6
Toby Cm <toby@vsus.app>
2024-05-21 17:55:17 -0700

    fix: :bug: fix redirecting to google form

--- src/components/Buttons/HomeTop.tsx
+++ src/components/Buttons/HomeTop.tsx
@@ -7,7 +7,22 @@ export default function HomeTopButton({ title, url }: { title: string; url: stri
   const navigate = useNavigate();
 
   return (
-    <Button bg="black" c="pr-yellow" size="4rem" fw="bold" fz="h3" m="md" mb="0" w="100%" variant="filled" radius="sm" onClick={() => navigate(url)}>
+    <Button
+      bg="black"
+      c="pr-yellow"
+      size="4rem"
+      fw="bold"
+      fz="h3"
+      m="md"
+      mb="0"
+      w="100%"
+      variant="filled"
+      radius="sm"
+      onClick={() => {
+        if (url.startsWith("/")) navigate(url);
+        else window.open(url, "_blank");
+      }}
+    >
       {title}
     </Button>
   );
