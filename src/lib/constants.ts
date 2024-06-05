9de92cac32d19074d2f4e668fbb0cbc70dc4ac72
Toby Cm <toby@vsus.app>
2024-06-04 18:24:59 -0700

    feat: :fire: finish Home page siuuuuuu

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const email = "penticton.robotics@gmail.com";
+export const mrWalkerEmail = "jwalker@sd67.bc.ca";
 
 export const competitionDate = new Date(1736010000000);
 
@@ -10,4 +11,17 @@ export const homeButtons: {
   { title: "Apply to Join", url: "https://docs.google.com/forms/d/e/1FAIpQLSdyxV6KFTOH4CrVNiCXxE-if7KObaapBJ_KSYYoopuZmHnKNw/viewform" },
   { title: "Sponsor/Donate", url: "/sponsor" },
   { title: "Event", url: "/events" },
+];
+
+export const sponsorFiles: string[] = [
+  "/images/sponsors/Inland.svg",
+  "/images/sponsors/Nufloors.svg",
+
+  "/images/sponsors/Box Mechanical.svg",
+  "/images/sponsors/Helix.svg",
+  "/images/sponsors/Nor-Mar Cranes.svg",
+  "/images/sponsors/Peerless.svg",
+  "/images/sponsors/Penticton Foundry.svg",
+
+  "/images/sponsors/Total Restoration.svg",
 ];
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Title } from "@mantine/core";
+import { Flex, Text, Title } from "@mantine/core";
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -13,17 +13,17 @@ export default function NotFound() {
   });
 
   return (
-    <Box w="100%" h="50vh" display="flex" style={{ flexDirection: "column", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
-      <Title mb={"md"} ta="center">
+    <Flex w="100%" h="50vh" direction="column" align="center" justify="center" ta="center">
+      <Title mb="md" ta="center">
         Oops, content not found!
       </Title>
       <Text ta="center">
         You will be automatically redirected to the{" "}
-        <Link to="/" style={{ color: "black" }}>
+        <Link to="/" color="black">
           homepage
         </Link>{" "}
         shortly.
       </Text>
-    </Box>
+    </Flex>
   );
 }
