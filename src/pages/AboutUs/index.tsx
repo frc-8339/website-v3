f7623b4d911531d99155d16de19a0eb820e13501
Toby Cm <toby@vsus.app>
2024-10-22 01:02:00 +0000

    fix: :fire: lil about us fix

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -86,12 +86,12 @@ export default function AboutUs() {
         <Text w="80%" mt="xl" fz="h2">
           We are Penticton Robotics, a Penticton based FIRST robotics team meeting out of Princess Margaret Secondary. The Penticton Robotics Club is
           a diverse, student-led team of high school students with the vision and dream of representing School District 67 at the regional FIRST
-          robotics competition on March 1st, 2023, in Victoria, British Columbia. Our challenge, as a team, is to dedicate hundreds of hours over the
-          next few months to producing a robot capable of sophisticated, dynamic movement with the ability to follow complex commands to satisfy a
-          list of tasks provided by the robotics competition. The large-scale event is known worldwide as FIRST or “For Inspiration and Recognition of
-          Science and Technology.” FIRST is a global nonprofit organization dedicated to preparing and leading the next generation of youth into the
-          world of science, technology, engineering, and mathematics. You can learn more about FIRST on the "<Link to="/first">What is FIRST?</Link>"
-          menu tab. <br />
+          robotics competition on February 26th - March 1st, in Vancouver, British Columbia. Our challenge, as a team, is to dedicate hundreds of
+          hours over the next few months to producing a robot capable of sophisticated, dynamic movement with the ability to follow complex commands
+          to satisfy a list of tasks provided by the robotics competition. The large-scale event is known worldwide as FIRST or “For Inspiration and
+          Recognition of Science and Technology.” FIRST is a global nonprofit organization dedicated to preparing and leading the next generation of
+          youth into the world of science, technology, engineering, and mathematics. You can learn more about FIRST on the "
+          <Link to="/first">What is FIRST?</Link>" menu tab. <br />
           <br />
           In our rookie year, at the regional competition in Victoria, our team, despite all odds, made it all the way to quarter-finals and was
           awarded the “2020 Rookie All-Star Team,” qualifing us for the World Championships in Houston, Texas. Unfortunately, our dreams were cut
@@ -102,7 +102,7 @@ export default function AboutUs() {
           associated with our trip to the Victoria competition. This year, we have a similar goal, we need to raise around $22,000. In order to reach
           this goal, we are seeking donations and sponsors. All donations and sponsorships are precious and gratefully received. Any contribution, big
           or small, is greatly appreciated and brings us one step closer to making our dreams a reality! Please consult our "
-          <Link to="/sponsor">Sponsor/Donate</Link>" page if you are interested in making a monetary contribution.
+          <Link to="/power">Sponsor/Donate</Link>" page if you are interested in making a monetary contribution.
         </Text>
       </Flex>
     </Flex>
